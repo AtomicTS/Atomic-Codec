345d9a716856369b55aced9f9e67a56ae7456427
Serial-V <divinfall@proton.me>
2025-12-10 21:17:05 -0500

    - Fixed export typings

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import { BufferReader } from "./BufferReader";
 import { BufferWriter } from "./BufferWriter";
-import { Events } from "./Events";
 import { Packet } from "./Packet";
 import { PacketRegistry } from "./PacketRegistry";
-import { PacketSerializer } from "./PacketSerializer";
 
-export {
-    BufferReader,
-    BufferWriter, Events, Packet,
-    PacketRegistry, PacketSerializer
-};
-
+export { BufferReader, BufferWriter, Packet, PacketRegistry };
+export type { Events } from "./Events";
+export type { PacketSerializer } from "./PacketSerializer";
