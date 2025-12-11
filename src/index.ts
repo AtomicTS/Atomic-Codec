92bd57b6b52d13be092e058ef384d86863738f68
Serial-V <divinfall@proton.me>
2025-12-10 22:21:31 -0500

    - Forgot to add packet registeration

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { BufferWriter } from "./BufferWriter";
 import { Packet } from "./Packet";
 import { PacketRegistry } from "./PacketRegistry";
 
-export { BufferReader, BufferWriter, Packet, PacketRegistry };
+import "./serializers/register";
+
 export type { Events } from "./Events";
 export type { PacketSerializer } from "./PacketSerializer";
+export { BufferReader, BufferWriter, Packet, PacketRegistry };
+
