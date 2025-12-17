98fa6b2e339bf3848179b6347e13e98dc1fff1fe
Serial-V <divinfall@proton.me>
2025-12-17 21:38:25 +0000

    - Fixed text packet

--- src/packets/text.ts
+++ src/packets/text.ts
@@ -1,30 +1,9 @@
-export type TextType =
-  | "raw"
-  | "chat"
-  | "translation"
-  | "popup"
-  | "jukebox_popup"
-  | "tip"
-  | "system"
-  | "whisper"
-  | "announcement"
-  | "json_whisper"
-  | "json"
-  | "json_announcement";
-
-export type TextCategory = "message_only" | "author_and_message" | "message_and_params" | number;
-
 export interface TextPacket {
-  category?: TextCategory;
-  type: TextType | number;
   needs_translation: boolean;
-  source_name?: string;
+  /** * Category: 0 (message_only), 1 (author_and_message), 2 (message_and_params) */
+  category: number | string;
+  type: number | string;
   message: string;
-  parameters?: string[];
-  xuid?: string;
-  platform_chat_id?: string;
-  filtered_message?: string;
-  // Optional raw fields from the protocol for completeness
   name_raw?: string;
   name_tip?: string;
   name_system?: string;
@@ -34,7 +13,12 @@ export interface TextPacket {
   name_chat?: string;
   name_whisper?: string;
   name_announcement?: string;
+  source_name?: string;
   name_translate?: string;
   name_popup?: string;
   name_jukebox_popup?: string;
+  parameters?: string[];
+  xuid: string;
+  platform_chat_id: string;
+  filtered_message?: string;
 }
